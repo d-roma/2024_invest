Fixed benefit computation, adding fake "sell" at current date, to have a fair comparison with B&H strategy. SAR strategy first time "better" than B&H, with more benefit and less drawdown.

SAR_optimize.py
@@ -1,13 +1,12 @@
-import numpy as np
-
 import matplotlib
+
 matplotlib.use('QtAgg')
 from matplotlib import pyplot as plt
 
 import numpy as np
 
-import yfinance as yf
 import pandas as pd
+import pandas_ta as ta
 ### Current version requires to comment line 86 of file pandas_ta\utils\data\yahoofinance.py
 ### as follows
 """
@@ -16,15 +15,15 @@ if Imports["yfinance"] and ticker is not None:
     # yfra.pdr_override()
 """
 ### https://twopirllc.github.io/pandas-ta/
-import pandas_ta as ta
 
-def SAR_benchmark(df, af, max_af,  af0=0.001):
+
+def SAR_benchmark(df, af, max_af, af0=0.001):
     df.ta.psar(af0=af0, af=af, max_af=max_af, append=True)
 
     PSARl_name = [i for i in df.columns if i.startswith('PSARl_')][0]
     PSARs_name = [i for i in df.columns if i.startswith('PSARs_')][0]
 
-    df.rename(columns={PSARl_name: 'PSARl', PSARs_name: 'PSARs' }, inplace=True)
+    df.rename(columns={PSARl_name: 'PSARl', PSARs_name: 'PSARs'}, inplace=True)
 
     df["buy_signal"] = 0
     df["sell_signal"] = 0
@@ -49,11 +48,14 @@ def SAR_benchmark(df, af, max_af,  af0=0.001):
             c_drawdown = (data - p_max) / p_max
             if c_drawdown < drawdown:
                 drawdown = c_drawdown
-            #if not np.isnan(row["PSARs"]) or (row.High < row['SMA_200']):
+            # if not np.isnan(row["PSARs"]) or (row.High < row['SMA_200']):
             if not np.isnan(row["PSARs"]):
                 status = 0
                 df.loc[index, "sell_signal"] = 1
         df.loc[index, "status"] = status
+
+    # Add a "fake" sell signal at the end to consider gain up to current date
+    df.loc[index, "sell_signal"] = 1
 
     index_buy = df.index[np.where(df["buy_signal"] > 0)]
     index_sell = df.index[np.where(df["sell_signal"] > 0)]
@@ -80,7 +82,7 @@ def SAR_benchmark(df, af, max_af,  af0=0.001):
 
 
 if __name__ == '__main__':
-    #period = '5y'
+    # period = '5y'
     period = 'max'
 
     print("###### Strategy 3 - SAR Optimization")
@@ -93,47 +95,51 @@ if __name__ == '__main__':
     df.ta.sma(length=100, append=True)
     df.ta.sma(length=200, append=True)
 
-    af_array = np.linspace(0.001, 0.01, 10)
+    af_array = np.linspace(0.0001, 0.005, 10)
     max_af_array = np.linspace(0.005, 0.1, 10)
+    af0_array = np.linspace(0.0001, 0.001, 10)
     results_benefit = np.empty((af_array.size * max_af_array.size))
     results_drawdown = np.empty((af_array.size * max_af_array.size))
     results_values = np.empty((af_array.size * max_af_array.size, 2))
+    max_af = 0.01
     for i, af in enumerate(af_array):
-        for j, max_af in enumerate(max_af_array):
-            benefit, drawdown = SAR_benchmark(df.copy(), af, max_af)
-            results_benefit[i*af_array.size + j] = benefit
-            results_drawdown[i*af_array.size + j] = drawdown
-            results_values[i*af_array.size + j, :] = (af, max_af)
-
+        #for j, max_af in enumerate(max_af_array):
+        for j, af0 in enumerate(af0_array):
+            benefit, drawdown = SAR_benchmark(df.copy(), af, max_af, af0=af0)
+            results_benefit[i * af_array.size + j] = benefit
+            results_drawdown[i * af_array.size + j] = drawdown
+            #results_values[i * af_array.size + j, :] = (af, max_af)
+            results_values[i * af_array.size + j, :] = (af, af0)
 
     print(results_benefit.max())
     print(results_drawdown.max())
     print(results_values[results_benefit.argmax()])
 
-    af_array = ["%.3f" % i for i in af_array]
-    max_af_array = ["%.3f" % i for i in max_af_array]
+    af_array = ["%.4f" % i for i in af_array]
+    #max_af_array = ["%.3f" % i for i in max_af_array]
+    af0_array = ["%.4f" % i for i in af0]
 
     fig, ax = plt.subplots()
     im = plt.imshow(results_benefit.reshape(len(af_array), len(max_af_array)))
     ax.set_xticks(np.arange(len(af_array)), labels=af_array)
-    ax.set_yticks(np.arange(len(max_af_array)), labels=max_af_array)
+    #ax.set_yticks(np.arange(len(max_af_array)), labels=max_af_array)
+    ax.set_yticks(np.arange(len(af0_array)), labels=af0_array)
     plt.setp(ax.get_xticklabels(), rotation=-30, ha="left",
              rotation_mode="anchor")
     plt.colorbar()
     plt.xlabel("AF")
-    plt.ylabel("MAX_AF")
-    plt.savefig("SAR_optimization_benefit.png",  bbox_inches='tight')
+    #plt.ylabel("MAX_AF")
+    plt.ylabel("AF0")
+    plt.savefig("SAR_optimization_benefit.png", bbox_inches='tight')
 
     fig, ax = plt.subplots()
     im = plt.imshow(results_drawdown.reshape(len(af_array), len(max_af_array)))
     ax.set_xticks(np.arange(len(af_array)), labels=af_array)
-    ax.set_yticks(np.arange(len(max_af_array)), labels=max_af_array)
+    #ax.set_yticks(np.arange(len(max_af_array)), labels=max_af_array)
+    ax.set_yticks(np.arange(len(af0_array)), labels=af0_array)
     plt.setp(ax.get_xticklabels(), rotation=-30, ha="left",
              rotation_mode="anchor")
     plt.colorbar()
     plt.savefig("SAR_optimization_drawdown.png", bbox_inches='tight')
 
     print("Finished")
-
-
-